14b7b876abae6197a733fd9050aec37bb4593e4d
Blair Williams <supercleanse@gmail.com>
2013-02-17 23:44:32 -0700

    Word

--- super-stripe.php
+++ super-stripe.php
@@ -15,121 +15,49 @@ if(!defined('ABSPATH')) {die('You are not allowed to call this page directly.');
 define('SUPSTR_PLUGIN_SLUG',plugin_basename(__FILE__));
 define('SUPSTR_PLUGIN_NAME',dirname(SUPSTR_PLUGIN_SLUG));
 define('SUPSTR_PATH',WP_PLUGIN_DIR.'/'.SUPSTR_PLUGIN_NAME);
-define('SUPSTR_IMAGES_PATH',SUPSTR_PATH.'/images');
-define('SUPSTR_CSS_PATH',SUPSTR_PATH.'/css');
-define('SUPSTR_JS_PATH',SUPSTR_PATH.'/js');
-define('SUPSTR_I18N_PATH',SUPSTR_PATH.'/i18n');
-define('SUPSTR_LIB_PATH',SUPSTR_PATH.'/app/lib');
-define('SUPSTR_APIS_PATH',SUPSTR_PATH.'/app/apis');
-define('SUPSTR_MODELS_PATH',SUPSTR_PATH.'/app/models');
-define('SUPSTR_CONTROLLERS_PATH',SUPSTR_PATH.'/app/controllers');
-define('SUPSTR_GATEWAYS_PATH',SUPSTR_PATH.'/app/gateways');
-define('SUPSTR_VIEWS_PATH',SUPSTR_PATH.'/app/views');
-define('SUPSTR_WIDGETS_PATH',SUPSTR_PATH.'/app/widgets');
-define('SUPSTR_HELPERS_PATH',SUPSTR_PATH.'/app/helpers');
 define('SUPSTR_URL',plugins_url($path = '/'.SUPSTR_PLUGIN_NAME));
-define('SUPSTR_IMAGES_URL',SUPSTR_URL.'/images');
-define('SUPSTR_CSS_URL',SUPSTR_URL.'/css');
-define('SUPSTR_JS_URL',SUPSTR_URL.'/js');
-define('SUPSTR_GATEWAYS_URL',SUPSTR_URL.'/app/gateways');
 define('SUPSTR_SCRIPT_URL',get_option('home').'/index.php?plugin=supstr');
 define('SUPSTR_OPTIONS_SLUG', 'supstr_options');
-define('SUPSTR_EDITION', '!!supercleanse!!');
 
-/**
- * Returns current plugin version.
- *
- * @return string Plugin version
- */
-function supstr_plugin_info($field) {
-  static $plugin_folder, $plugin_file;
+class Supstr {
+  public function __construct() {
+    if( is_admin() ) {
+      add_action( 'admin_menu', array($this,'admin_menu') );
+      //add_action( 'admin_init', 'register_mysettings' );
+    } else {
+      // non-admin enqueues, actions, and filters
+    }
+  } 
 
-  if( !isset($plugin_folder) or !isset($plugin_file) ) {
-    if( ! function_exists( 'get_plugins' ) )
-      require_once( ABSPATH . '/wp-admin/includes/plugin.php' );
-    
-    $plugin_folder = get_plugins( '/' . plugin_basename( dirname( __FILE__ ) ) );
-    $plugin_file = basename( ( __FILE__ ) );
+  public function admin_menu() {
+    add_menu_page(__('Super Stripe', 'super-stripe'), __('Super Stripe', 'super-stripe'), 'administrator', 'super-stripe', array($this,'settings_page'), SUPSTR_URL. '/icon.png');
+    add_action( 'admin_init', array($this,'register_settings') );
   }
 
-  if(isset($plugin_folder[$plugin_file][$field]))
-    return $plugin_folder[$plugin_file][$field];
+  public function register_settings() {
+    register_setting( 'supstr-settings-group', 'supstr_license_key' );
+  }
 
-  return '';
-}
+  public function settings_page() {
+?>
+<div class="wrap">
+<h2><?php _e('Super Stripe'); ?></h2>
 
-// Plugin Information from the plugin header declaration
-define('SUPSTR_VERSION', supstr_plugin_info('Version'));
-define('SUPSTR_DISPLAY_NAME', supstr_plugin_info('Name'));
-define('SUPSTR_AUTHOR', supstr_plugin_info('Author'));
-define('SUPSTR_AUTHOR_URI', supstr_plugin_info('AuthorURI'));
-define('SUPSTR_DESCRIPTION', supstr_plugin_info('Description'));
-
-// Autoload all the requisite classes
-function supstr_autoloader($class_name)
-{
-  // Only load SuperStripe classes here
-  if(preg_match('/^Supstr.+$/', $class_name))
-  {
-    if(preg_match('/^SupstrBase.+$/', $class_name)) // Base classes are in lib
-      $filepath = SUPSTR_LIB_PATH."/{$class_name}.php";
-    else if(preg_match('/^.+Controller$/', $class_name))
-      $filepath = SUPSTR_CONTROLLERS_PATH."/{$class_name}.php";
-    else if(preg_match('/^.+Helper$/', $class_name))
-      $filepath = SUPSTR_HELPERS_PATH."/{$class_name}.php";
-    else if(preg_match('/^.+Exception$/', $class_name))
-      $filepath = SUPSTR_LIB_PATH."/SupstrExceptions.php";
-    else if(preg_match('/^.+Gateway$/', $class_name)) {
-      foreach( SupstrGatewayFactory::paths() as $path ) {
-        $filepath = $path."/{$class_name}.php";
-        if( file_exists($filepath) ) {
-          include_once($filepath); return;
-        }
-      }
-      return;
-    }
-    else {
-      $filepath = SUPSTR_MODELS_PATH."/{$class_name}.php";
+<form method="post" action="options.php">
+  <?php settings_fields( 'supstr-settings-group' ); ?>
+  <?php do_settings( 'supstr-settings-group' ); ?>
+  <table class="form-table">
+    <tr valign="top">
+      <th scope="row"><?php _e('Super Stripe License Key'); ?></th>
+      <td><input type="text" name="supstr_license_key" value="<?php echo get_option('supstr_license_key'); ?>" /></td>
+    </tr>
+    </table>
     
-      // Now let's try the lib dir if its not a model
-      if(!file_exists($filepath))
-        $filepath = SUPSTR_LIB_PATH."/{$class_name}.php";
-    }
-    
-    if(file_exists($filepath))
-      include_once($filepath);
+    <?php submit_button(); ?>
+</form>
+</div>
+<?php
   }
 }
 
-// if __autoload is active, put it on the spl_autoload stack
-if(is_array(spl_autoload_functions()) and in_array('__autoload', spl_autoload_functions()))
-  spl_autoload_register('__autoload');
-
-// Add the autoloader
-spl_autoload_register('supstr_autoloader');
-
-// Gotta load the language before everything else
-SupstrAppController::load_language();
-
-// Load our controllers
-$controllers = @glob( SUPSTR_CONTROLLERS_PATH . '/*', GLOB_NOSORT );
-foreach( $controllers as $controller ) {
-  $class = preg_replace( '#\.php#', '', basename($controller) );
-  if( preg_match( '#Supstr.*Controller#', $class ) )
-    $obj = new $class;
-}
-
-// Setup screens
-SupstrAppController::setup_menus();
-
-// Include Widgets
-
-// Register Widgets
-
-// Include APIs
-
-// Template Tags
-
-register_activation_hook( SUPSTR_PLUGIN_SLUG, create_function( '', 'require_once( SUPSTR_LIB_PATH . "/activation.php");' ) );
-register_deactivation_hook( SUPSTR_PLUGIN_SLUG, create_function( '', 'require_once( SUPSTR_LIB_PATH . "/deactivation.php");' ) );
-//register_uninstall_hook( SUPSTR_PLUGIN_SLUG, create_function( '', 'require_once( SUPSTR_PATH . "/uninstall.php");' ) );
+new Supstr();
