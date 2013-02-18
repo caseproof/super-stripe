34b4e7431887133ccce0074021f4118b51008512
Blair Williams <supercleanse@gmail.com>
2013-02-18 14:13:39 -0700

    Just committing for state

--- super-stripe.php
+++ super-stripe.php
@@ -22,41 +22,52 @@ define('SUPSTR_OPTIONS_SLUG', 'supstr_options');
 class Supstr {
   public function __construct() {
     if( is_admin() ) {
-      add_action( 'admin_menu', array($this,'admin_menu') );
-      //add_action( 'admin_init', 'register_mysettings' );
+      add_action( 'admin_menu', array( $this, 'admin_menu' ) );
+      add_action( 'admin_init', array( $this, 'register_settings' ) );
     } else {
       // non-admin enqueues, actions, and filters
     }
   } 
 
   public function admin_menu() {
-    add_menu_page(__('Super Stripe', 'super-stripe'), __('Super Stripe', 'super-stripe'), 'administrator', 'super-stripe', array($this,'settings_page'), SUPSTR_URL. '/icon.png');
+    add_options_page(__('Super Stripe', 'super-stripe'), __('Super Stripe', 'super-stripe'), 'administrator', 'super-stripe', array($this,'settings_page'));
     add_action( 'admin_init', array($this,'register_settings') );
   }
 
   public function register_settings() {
     register_setting( 'supstr-settings-group', 'supstr_license_key' );
+    add_settings_section( 'supstr-settings', __('Super Stripe'), array($this,'settings_page'), 'super-stripe' );
+    add_settings_field( 'supstr-license-key', __('License Key'), array($this,'render_license_key_field'), 'super-stripe', 'supstr-settings' );
+  }
+
+  public function render_license_key_field() {
+    ob_start();
+
+    $val = esc_attr( get_option( 'supstr_license_key' ) );
+    ?>
+    <input type="text" name="supstr_license_key" value="<?php echo $val; ?>" />
+    <?php
+
+    $content = ob_get_contents();
+    ob_end_clean();
+
+    return $content;
   }
 
   public function settings_page() {
-?>
-<div class="wrap">
-<h2><?php _e('Super Stripe'); ?></h2>
+  ?>
 
-<form method="post" action="options.php">
-  <?php settings_fields( 'supstr-settings-group' ); ?>
-  <?php do_settings( 'supstr-settings-group' ); ?>
-  <table class="form-table">
-    <tr valign="top">
-      <th scope="row"><?php _e('Super Stripe License Key'); ?></th>
-      <td><input type="text" name="supstr_license_key" value="<?php echo get_option('supstr_license_key'); ?>" /></td>
-    </tr>
-    </table>
+  <div class="wrap">
+    <h2><?php _e('Super Stripe'); ?></h2>
     
-    <?php submit_button(); ?>
-</form>
-</div>
-<?php
+    <form method="post" action="options.php">
+      <?php settings_fields( 'supstr-settings-group' ); ?>
+      <?php do_settings_sections( 'super-stripe' ); ?>
+      <?php //submit_button(); ?>
+    </form>
+  </div>
+
+  <?php
   }
 }
 
