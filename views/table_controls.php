fdc28f0125ddd39b7c7f57a12ee2e099224a0221
Cartpauj <paul.carter@cartpauj.com>
2013-02-19 00:00:47 -0700

    oops

--- views/table_controls.php
+++ views/table_controls.php
@@ -4,14 +4,14 @@ $search = (isset($_REQUEST['search']) && !empty($_REQUEST['search']))?$_REQUEST[
 $perpage = (isset($_REQUEST['perpage']) && !empty($_REQUEST['perpage']))?$_REQUEST['perpage']:10;
 ?>
 
-<input id="cspf-table-search" value="<?php echo $search; ?>" data-value="<?php _e('Search ...', 'memberpress'); ?>" /><br/><br/>
+<input id="cspf-table-search" value="<?php echo $search; ?>" data-value="<?php _e('Search ...', 'super-stripe'); ?>" /><br/><br/>
 <div id="table-actions">
-  <?php _e('Display', 'memberpress'); ?>&nbsp;
+  <?php _e('Display', 'super-stripe'); ?>&nbsp;
   <select id="cspf-table-perpage">
     <option value="10"<?php selected(10, $perpage); ?>>10</option>
     <option value="25"<?php selected(25, $perpage); ?>>25</option>
     <option value="50"<?php selected(50, $perpage); ?>>50</option>
     <option value="100"<?php selected(100, $perpage); ?>>100&nbsp;</option>
   </select>&nbsp;
-  <?php _e('entries', 'memberpress'); ?>
+  <?php _e('entries', 'super-stripe'); ?>
 </div>
