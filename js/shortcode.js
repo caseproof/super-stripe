c7a51350aaeb0ff564a5b2bf80459bac8f4e77e6
Cartpauj <paul.carter@cartpauj.com>
2013-02-20 21:41:01 -0700

    fix validate JS issue

---
+++ js/shortcode.js
@@ -0,0 +1,12 @@
+(function($) {
+  $(document).ready(function() {
+    //Fire off the validator
+    $('.supstr-shortcode-form').validate({
+      submitHandler: function(form, validator) {
+        //Show loading image if input is valid and submit is triggered
+        $('.supstr_loading_gif').show();
+        form.submit();
+      }
+    });
+  });
+})(jQuery);
