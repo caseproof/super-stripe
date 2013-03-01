69c52b096272bb2fd3b01c09315eb795c44aa69e
Blair Williams <supercleanse@gmail.com>
2013-02-28 22:52:26 -0700

    Changed titles in admin

--- views/display_form.php
+++ views/display_form.php
@@ -1,7 +1,7 @@
 <?php if(!defined('ABSPATH')) {die('You are not allowed to call this page directly.');} ?>
 
 <div class="wrap">
-  <h2><?php _e('Super Stripe'); ?></h2>
+  <h2><?php _e('Super Stripe Options'); ?></h2>
   <br/>
   <?php require(SUPSTR_PATH.'/views/errors.php'); ?>
   <form method="post" action="">
--- views/txn_list.php
+++ views/txn_list.php
@@ -3,7 +3,6 @@ if(!defined('ABSPATH')) {die('You are not allowed to call this page directly.');
 ?>
 
 <div class="wrap">
-  <div class="icon32"></div>
-  <h2><?php _e('Transactions', 'super-stripe'); ?></h2>
+  <h2><?php _e('Super Stripe Transactions', 'super-stripe'); ?></h2>
   <?php $sub_table->display(); ?>
 </div>
